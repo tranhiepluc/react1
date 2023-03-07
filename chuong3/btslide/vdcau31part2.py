96a0ee43a7ce2b9dccaad97a5e9cb9014459baaf
tranhiepluc <tranhiepluc>
2023-03-07 14:51:02 +0700

    luc

---
+++ chuong3/btslide/vdcau31part2.py
@@ -0,0 +1,6 @@
+n=int(input("nhap tu ban phim mot so nguyen n tu 1 - 50: "))
+while n<1 or n>50:
+    print("n khong thuoc khoang tren, yeu cau nhap lai")
+    n=int(input("nhap tu ban phim mot so nguyen tu 1 - 50: "))
+    
+    
